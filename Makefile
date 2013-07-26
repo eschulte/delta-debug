3b7a7409d18888b73d2058c1d8a08468e9725be6
Eric Schulte <schulte.eric@gmail.com>
2013-07-25 19:56:42 -0600

    tested executable

--- Makefile
+++ Makefile
@@ -5,6 +5,7 @@ CL_SETUP=cl-launch-setup.lisp
 CLFLAGS=--no-include --system delta-debug-exe --lisp $(LISP) --dump '!' -f $(CL_SETUP)
 
 all: delta
+.PHONY: check clean
 
 $(CL_SETUP):
 	echo "(load \"$(QUICK_LISP)/setup.lisp\")" >$@
@@ -12,5 +13,18 @@ $(CL_SETUP):
 delta: delta-debug-exe.lisp $(CL_SETUP)
 	$(CLC) $(CLFLAGS) --output $@ -r delta-debug-exe:main
 
+test-script.sh:
+	@echo "#!/bin/sh" >$@
+	@echo 'sh $$@ >/dev/null' >>$@
+	@echo "if [ \$$? -eq 89 ];then exit 0;else exit 1;fi" >>$@
+	@chmod +x $@
+
+test-file.sh:
+	@echo "#!/bin/sh" >$@
+	@seq 100|sed 's/^/echo /;89s/echo/exit/' >>$@
+
+check: delta test-script.sh test-file.sh
+	@if [ "$$(./$^)" == "exit 89" ];then echo "PASS"; else echo "FAIL";fi
+
 clean:
-	rm -f delta $(CL_SETUP) *.fasl *.lx32fsl
+	rm -f delta $(CL_SETUP) test-file.sh test-script.sh *.fasl *.lx32fsl
--- delta-debug-exe.lisp
+++ delta-debug-exe.lisp
@@ -7,6 +7,10 @@
 ;;; Code:
 (in-package :delta-debug-exe)
 
+(defvar out t "Output stream to write results.")
+(defvar verbose nil "Verbose output")
+(defvar keep nil "Keep temporary files")
+
 (defmacro getopts (&rest forms)
   (let ((arg (gensym)))
     `(loop :for ,arg = (pop args) :while ,arg :do
@@ -15,35 +19,79 @@
                       `((or (string= ,arg ,short) (string= ,arg ,long)) ,@body))
                     forms)))))
 
+#+sbcl
+(locally (declare (sb-ext:muffle-conditions sb-ext:compiler-note))
+  (sb-alien:define-alien-routine (#-win32 "tempnam" #+win32 "_tempnam" tempnam)
+      sb-alien:c-string
+    (dir sb-alien:c-string)
+    (prefix sb-alien:c-string)))
+
+#+ccl
+(defun tempnam (dir prefix)
+  (ccl:with-filename-cstrs ((base dir) (prefix prefix))
+    (ccl:get-foreign-namestring (#_tempnam base prefix))))
+
+(defun temp-file-name (&optional ext)
+  (let ((base #+clisp
+          (let ((stream (gensym)))
+            (eval `(with-open-stream (,stream (ext:mkstemp nil))
+                     (pathname ,stream))))
+          #+(or sbcl ccl)
+          (tempnam nil nil)
+          #-(or sbcl clisp ccl)
+          (error "no temporary file backend for this lisp.")))
+    (if ext
+        (concatenate 'string base "." ext)
+        base)))
+
+(defmacro with-temp-file (spec &rest body)
+  "SPEC holds the variable used to reference the file w/optional extension.
+After BODY is executed the temporary file is removed."
+  `(let ((,(car spec) (temp-file-name ,(second spec))))
+     (unwind-protect (progn ,@body)
+       (when (and (probe-file ,(car spec)) (not keep))
+         (delete-file ,(car spec))))))
+
 (defun main (&optional (args *arguments*))
   (when (or (not args) (< (length args) 2)
             (string= (subseq (car args) 0 2) "-h")
             (string= (subseq (car args) 0 3) "--h"))
-    (format t "Usage: delta FILE TEST-SCRIPT
+    (format t "Usage: delta TEST-SCRIPT FILE
  Minimize the contents of FILE as much as possible,
  such that TEST-SCRIPT continues to exit successfully
  when passed FILE as its only argument.
 
 Options:
- -o,--out FILE ------ write output to FILE~%") (quit))
+ -o,--out FILE ------ write output to FILE
+ -k,--keep ---------- keep temporary files
+ -v,--verbose ------- verbose output~%") (quit))
 
-  (let ((original (split-sequence #\Newline
+  (let ((script (pop args))
+        (original (split-sequence #\Newline
                     (with-open-file (in (pop args))
                       (let ((seq (make-string (file-length in))))
                         (read-sequence seq in)
-                        seq))))
-        (script (pop args))
-        (out t))
+                        seq)))))
 
     (getopts
-     ("-o" "--out" (setf out (open (pop args) :direction :output))))
+     ("-o" "--out"     (setf out (open (pop args) :direction :output)))
+     ("-v" "--verbose" (setf verbose t))
+     ("-k" "--keep"    (setf keep t)))
 
-    (format out "~{~a~^~%~}"
+    (format out "~{~a~^~%~}~%"
             (minimize original
-                      (lambda (file)
-                        (multiple-value-bind (out err errno)
-                            (shell-command (format nil "~a ~a"  script file))
-                          (declare (ignorable out err))
-                          errno))))
+                      (lambda (contents)
+                        (with-temp-file (file)
+                          (with-open-file (out file :direction :output)
+                            (format out "~{~a~^~%~}~%" contents))
+                          (multiple-value-bind (out err errno)
+                              (shell-command
+                               (if (pathname-directory (pathname script))
+                                   (format nil "~a ~a" script file)
+                                   (format nil "./~a ~a" script file)))
+                            (declare (ignorable out err))
+                            (when verbose
+                              (format t ";; ~a ~a -> ~a~%" script file errno))
+                            (zerop errno))))))
 
     (when (streamp out) (close out))))
